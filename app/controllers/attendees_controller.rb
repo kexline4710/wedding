5ff5322fc12add3fa10a1706a81082404ba055ed
katy <kathryn.exline@gmail.com>
2016-11-29 13:17:24 -0600

    add basic CRUD actions and views for attendees

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,7 +1,40 @@
 class AttendeesController < ApplicationController
+  before_action :find_attendee, only: [:edit, :show, :update]
 
   def index
     @attendees = Attendee.all
+  end
+
+  def new
+    @attendee = Attendee.new
+  end
+
+  def create
+    @attendee = Attendee.create(attendee_params)
+
+    redirect_to @attendee
+  end
+
+  def edit
+  end
+
+  def update
+    @attendee.update(attendee_params)
+
+    redirect_to @attendee
+  end
+
+  def show
+  end
+
+  private
+
+  def attendee_params
+    params.require(:attendee).permit(:first_name, :last_name, :email, :full_address)
+  end
+
+  def find_attendee
+    @attendee = Attendee.find_by_id(params[:id])
   end
 
 end
--- spec/controllers/attendees_controller_spec.rb
+++ spec/controllers/attendees_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe AttendeesControllerController, type: :controller do
+RSpec.describe AttendeesController, type: :controller do
+  describe "#show" do
+    let(:attendee) { FactoryGirl.create(:attendee) }
 
+    it "is successful" do
+      get :show, id: attendee
+      expect(response.status).to eq(200)
+    end
+  end
 end
