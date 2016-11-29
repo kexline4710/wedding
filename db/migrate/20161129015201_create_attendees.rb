bb6ba59a3bad7246b817c765d0ea54deac765787
katy <kathryn.exline@gmail.com>
2016-11-28 19:56:32 -0600

    add attendee scaffolding

---
+++ db/migrate/20161129015201_create_attendees.rb
@@ -0,0 +1,12 @@
+class CreateAttendees < ActiveRecord::Migration
+  def change
+    create_table :attendees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :full_address
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/controllers/attendees_controller_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe AttendeesControllerController, type: :controller do
+
+end
