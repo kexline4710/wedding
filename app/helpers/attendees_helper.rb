6a57d0459c20aef365ded0e0acc7fc129f24dd7b
katy <kathryn.exline@gmail.com>
2016-11-29 13:15:24 -0600

    fix incorrect naming from scaffolding error

--- app/helpers/attendees_helper.rb
+++ app/helpers/attendees_helper.rb
@@ -1,2 +1,2 @@
-module AttendeesControllerHelper
+module AttendeesHelper
 end
--- spec/helpers/attendees_helper_spec.rb
+++ spec/helpers/attendees_helper_spec.rb
@@ -10,6 +10,5 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe AttendeesControllerHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe AttendeesHelper, type: :helper do
 end
