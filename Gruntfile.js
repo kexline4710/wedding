1969004f9f40ebe1bc94d6e8f12914a954259051
katy <kathryn.exline@gmail.com>
2016-12-01 13:10:23 -0600

    add grunt task to start rails server

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,21 @@ module.exports = function(grunt) {
 
   // Project configuration
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    exec: {
+      server: {
+        command: "rails server"
+      }
+    }
   });
 
-  // Default task
+  // Load npm tasks
+  grunt.loadNpmTasks('grunt-exec');
+
+  // Register tasks
   grunt.registerTask('default', 'Default task', function() {
     grunt.log.write('This is your default task').ok();
   });
 
+  grunt.registerTask('server', ['exec:server'])
 }
